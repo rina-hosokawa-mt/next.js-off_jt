faff20ec0f1ec4011eb7319c0dc08efa6245cf95
rina-hosokawa-mt <rina.hosokawa@metateam.co.jp>
2025-06-09 19:24:06 +0900

    gemini

---
+++ public/gemini.css
@@ -0,0 +1,14 @@
+let count = 0;
+const counterElement = document.getElementById("counter");
+const increaseButton = document.getElementById("increase");
+const decreaseButton = document.getElementById("decrease");
+
+increaseButton.addEventListener("click", () => {
+  count++;
+  counterElement.textContent = count;
+});
+
+decreaseButton.addEventListener("click", () => {
+  count--;
+  counterElement.textContent = count;
+});
