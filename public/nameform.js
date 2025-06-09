62d24870c911a64f4c7f7808811a25fa38b42364
rina-hosokawa-mt <rina.hosokawa@metateam.co.jp>
2025-06-09 17:43:14 +0900

    fb修正_アロー関数

--- public/nameform.js
+++ public/nameform.js
@@ -1,13 +1,12 @@
-const nameInput = document.getElementById("nameInput");
-const checkButton = document.getElementById("checkButton");
-const message = document.getElementById("message");
+const handleClick = () => {
+  const value = document.getElementById("nameInput").value.trim();
+  const message = document.getElementById("message");
 
-checkButton.addEventListener("click", () => {
-  const name = nameInput.value.trim();
-
-  if (name === "") {
+  if (value === "") {
     message.innerText = "名前を入力してください";
   } else {
-    message.innerText = `こんにちは、${name}さん！`;
+    message.innerText = `こんにちは、${value}さん！`;
   }
-});
+};
+
+document.getElementById("checkButton").addEventListener("click", handleClick);
